15b4730420339974c41ac4e9d3414c8d8a86281d
NicatGadirli <nicatqedirli65@gmail.com>
2023-06-01 02:12:15 +0400

    html css js

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,20 @@ function openprofileBar() {
     overlay.classList.remove("active");
   };
 }
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
